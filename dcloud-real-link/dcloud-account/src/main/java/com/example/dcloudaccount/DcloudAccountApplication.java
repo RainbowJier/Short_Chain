71691ac35f129a98feeeff1cb97cd4be7612b06b
RianbowJier <3021809270@qq.com>
2024-08-18 16:35:07 +0800

    :art: Enable Register and Discovery

--- dcloud-real-link/dcloud-account/src/main/java/com/example/dcloudaccount/DcloudAccountApplication.java
+++ dcloud-real-link/dcloud-account/src/main/java/com/example/dcloudaccount/DcloudAccountApplication.java
@@ -1,11 +1,19 @@
 package com.example.dcloudaccount;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+@MapperScan("com.example.dcloudaccount.mapper")
+@EnableTransactionManagement  // Open Database Transaction.
+@EnableFeignClients          // Enable OpenFeign
+@EnableDiscoveryClient      // Enable Service register and discovery.
 @SpringBootApplication
 public class DcloudAccountApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DcloudAccountApplication.class, args);
     }
